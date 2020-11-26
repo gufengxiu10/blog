6d90bd5599f3ad0d51c366bc419ac94e44463975
anng <296718933@qq.com>
2020-11-27 01:35:29 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 createApp(App)
   .use(store)
